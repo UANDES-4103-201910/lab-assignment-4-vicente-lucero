0624efaa86fee7018b28e6e9661135c7244f66b3
Your Name <you@example.com>
2019-04-03 11:28:01 -0300

    Event Venues and start_date Events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,16 @@
 class Event < ApplicationRecord
   belongs_to :event_venue
   has_many :ticket_types
+
+  validate :start_date_later_than_today
+
+  def start_date_later_than_today
+    if start_date.present? && start_date < Date.today
+      errors.add(:start_date, "Start date must be later than today")
+    end
+  end
+
+  #def one_event_at_a_time_for_each_venue
+
+
 end
--- app/models/event_venue.rb
+++ app/models/event_venue.rb
@@ -1,2 +1,4 @@
 class EventVenue < ApplicationRecord
+  validates :name, length: { minimum: 20 }
+  validates :capacity, numericality: {only_integer: true, greater_than: 0}
 end
