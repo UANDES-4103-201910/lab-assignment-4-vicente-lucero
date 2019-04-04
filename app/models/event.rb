b1a76fd8ab6008f2f61eaa7dfb02850680ef883a
Your Name <you@example.com>
2019-04-04 16:31:04 -0300

    finish lab

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
   has_many :ticket_types
 
   validate :start_date_later_than_today
-  before_create :one_event_at_a_time_for_each_venue
+  validate :one_event_at_a_time_for_each_venue
 
   def start_date_later_than_today
     if start_date.present? && start_date < Date.today
@@ -12,8 +12,8 @@ class Event < ApplicationRecord
   end
 
   def one_event_at_a_time_for_each_venue
-    a = Event.joins(event_venue: :start_date).count
-    if a > 1
+    a = Event.where(start_date: start_date).where(event_venue: event_venue).count
+    if a > 0
         errors.add(:event, "There cannot be two or more events hosted in the same venue with the same start date")
      end
   end
